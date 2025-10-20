9285022a4c6321f3ed4730963fd4935dd3432b1b
SethButler-STUDENT <g00467621@atu.ie>
2025-10-20 09:37:46 +0100

    update to main

--- Week6OOP/src/main/java/ie/atu/week6/TryMain.java
+++ Week6OOP/src/main/java/ie/atu/week6/TryMain.java
@@ -2,13 +2,15 @@ package ie.atu.week6;
 import java.util.Scanner;
 
 public class TryMain {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner sc = new Scanner(System.in);
         int age = 0;
-        while(true)
-        {
-            System.out.print("Enter your age: ");
-            String text = sc.nextLine().trim();
-        }
+
+        System.out.print("Enter your age: ");
+        String text = sc.nextLine().trim();
+
+        age = Integer.parseInt(text);
+        System.out.print("Thanks! You entered age = " + age);
     }
 }
