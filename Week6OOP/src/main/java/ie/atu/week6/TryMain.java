62e8466fe8596d051dd3c34b88cef78572ab71d8
SethButler-STUDENT <g00467621@atu.ie>
2025-10-20 09:27:58 +0100

    initial commit

---
+++ Week6OOP/src/main/java/ie/atu/week6/TryMain.java
@@ -0,0 +1,14 @@
+package ie.atu.week6;
+import java.util.Scanner;
+
+public class TryMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = 0;
+        while(true)
+        {
+            System.out.print("Enter your age: ");
+            String text = sc.nextLine().trim();
+        }
+    }
+}
